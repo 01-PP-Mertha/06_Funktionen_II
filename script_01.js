a1df1bf14e8d35199e3e19bcae50f305f25c844f
Mertha89 <mertha@live.de>
2023-05-12 09:37:43 +0200

    fertig

--- script_01.js
+++ script_01.js
@@ -13,24 +13,24 @@
 
 // module: addition a + b |  test:
 
-output(add(2,2));
-output(add(2,-2));
-output(add(2,0));
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
 
-function add(a, b) {
-	let c = a + b;
-	return c;
+// function add(a, b) {
+// 	let c = a + b;
+// 	return c;
 	
-}
+// }
 
 
 
-// module: output | test:
-// output("hello");
-// output(2);
-function output(outputData) {
-	console.log(outputData);
-}
+// // module: output | test:
+// // output("hello");
+// // output(2);
+// function output(outputData) {
+// 	console.log(outputData);
+// }
 
 
 
@@ -39,55 +39,55 @@ function output(outputData) {
 
 // // Dateneingabe und Überprüfung
 
-// let a = prompt("Gib den ersten Wert ein:");
-// let operation = prompt("Wähle eine Rechenart (+, -, *, /):");
-// let b = prompt("Gib den zweiten Wert ein:");
+let a = prompt("Gib den ersten Wert ein:");
+let operation = prompt("Wähle eine Rechenart (+, -, *, /):");
+let b = prompt("Gib den zweiten Wert ein:");
 
-// if(isNaN(a) && isNaN(b)){
-// console.log("Fehler: Ungültige Eingabe!");
-// } else {
+if(isNaN(a) || isNaN(b)){
+console.log("Fehler: Ungültige Eingabe!");
+} else {
 
 
-// // Funktionen für Grundrechenarten
+// Funktionen für Grundrechenarten
 
-// function add(a,b){
-// return parseFloat(a) + parseFloat(b);
-// }
+function add(a,b){
+return parseFloat(a) + parseFloat(b);
+}
 
-// function subtract(a,b){
-// return parseFloat(a) - parseFloat(b);
-// }
+function subtract(a,b){
+return parseFloat(a) - parseFloat(b);
+}
 
-// function multiply(a,b){
-// return parseFloat(a) * parseFloat(b);
-// }
+function multiply(a,b){
+return parseFloat(a) * parseFloat(b);
+}
 
-// function divide(a,b){
-// return parseFloat(a) / parseFloat(b);
-// }
+function divide(a,b){
+return parseFloat(a) / parseFloat(b);
+}
 
-// //Berechnung des Ergebnisses und Ausgabe in der Konsole
+//Berechnung des Ergebnisses und Ausgabe in der Konsole
 
-// let result;
-// switch(operation){
-// case "+":
-// result = add(a,b);
-// break;
-// case "-":
-// result = subtract(a,b);
-// break;
-// case "*":
-// result = multiply(a,b);
-// break;
-// case "/":
-// result = divide(a,b);
-// break;
-// default:
-// console.log("Fehler: Ungültige Rechenart!");
-// break;
-// }
+let result;
+switch(operation){
+case "+":
+result = add(a,b);
+break;
+case "-":
+result = subtract(a,b);
+break;
+case "*":
+result = multiply(a,b);
+break;
+case "/":
+result = divide(a,b);
+break;
+default:
+console.log("Fehler: Ungültige Rechenart!");
+break;
+}
 
-// if(result !== undefined){
-// console.log("Ergebnis: " + result);
-// }
-// }
+if(result !== undefined){
+console.log("Ergebnis: " + result);
+}
+}
